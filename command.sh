83c1cea2ea236fd15a46d6574754b03fd22de958
BCschwifty <98436836+BCschwifty@users.noreply.github.com>
2023-11-02 10:02:07 -0300

    Update command.sh

--- command.sh
+++ command.sh
@@ -36,20 +36,127 @@ sleap 5
 
 echo "setting up custom chl command"
 
+# Check if sudo is installed
+if command -v sudo &> /dev/null; then
+    echo "sudo is already installed."
+else
+    echo "sudo is not installed. Installing it..."
+    
+    # Use the appropriate package manager based on your Linux distribution.
+    # For Debian-based systems (e.g., Ubuntu):
+    # sudo apt-get install sudo
+
+    # For Red Hat-based systems (e.g., CentOS):
+    # sudo yum install sudo
+
+    # For other package managers, you may need to modify the installation command.
+
+    # For demonstration purposes, we'll use apt-get for Debian-based systems.
+    if command -v apt-get &> /dev/null; then
+        sudo apt-get update
+        sudo apt-get install sudo
+    else
+        echo "Your package manager is not supported. Please install sudo manually."
+    fi
+
+    # Verify if sudo installation was successful
+    if command -v sudo &> /dev/null; then
+        echo "sudo has been successfully installed."
+    else
+        echo "Failed to install sudo. Please install it manually."
+    fi
+fi
+
+# Check if whiptail is available
+if ! command -v whiptail &> /dev/null; then
+    echo "Whiptail is not installed. Please install it to use this script."
+    exit 1
+fi
+
+# Function to enable custom login colors
+enable_custom_login_colors() {
+    # Function to set the Bash prompt color
+set_prompt_color() {
+    local color_code="$1"
+    PS1="\[\e[${color_code}m\]\u\[\e[0m\]@\h:\w\$ "
+}
+
+# Function to map color names to color codes
+get_color_code() {
+    local color_name="$1"
+    case "$color_name" in
+        "black") echo "30" ;;
+        "red") echo "31" ;;
+        "green") echo "32" ;;
+        "yellow") echo "33" ;;	
+        "blue") echo "34" ;;
+        "magenta") echo "35" ;;
+        "cyan") echo "36" ;;
+        "white") echo "37" ;;
+        *) echo "$color_name is not a valid color name."; exit 1 ;;
+    esac
+}
+
+# Main script
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <user_color> <root_color>"
+    echo "Available color names: black, red, green, yellow, blue, magenta, cyan, white"
+    exit 1
+fi
+
+user_color="$1"
+root_color="$2"
+
+# Get color codes for the provided color names
+user_color_code=$(get_color_code "$user_color")
+root_color_code=$(get_color_code "$root_color")
+
+# Set the prompt color for the regular user
+set_prompt_color "$user_color_code"
+
+# Check if the user is root and set the prompt color accordingly
+if [ "$(whoami)" = "root" ]; then
+    set_prompt_color "$root_color_code"
+fi
+
+# Apply the changes to the current session
+source ~/.bashrc
+
+# Check if the script is run as 'su'
+if [ -n "$SU_USER" ]; then
+    echo "Switched to root user. Root user's color set to $root_color."
+else
+    echo "Regular user's color set to $user_color."
+fi
+    # You can specify the script name or perform any other actions.
+    
+    echo "Custom login colors enabled."
+    # Add your custom logic here.
+}
+
+# Check if the user wants to enable custom login colors
+if whiptail --yesno "Do you want to enable custom login colors?" 10 40; then
+    enable_custom_login_colors
+else
+    whiptail --msgbox "Custom login colors will not be enabled." 10 40
+    exit
+fi
+
+sudo su
 #removing previously install directories
 sudo rm -fr /scripts
 sudo rm /usr/local/bin/chl
 
 #setting up new command
-sudo su
 cd /
 sudo mkdir scripts
 cd /scripts
-sudo wget https://github.com/BCschwifty/script-dump/releases/download/run-v1/run.sh
+sudo wget https://github.com/BCschwifty/CHL-command/releases/download/CHL-1.0/run.sh
 
 cd /usr/local/bin
-sudo wget https://github.com/BCschwifty/script-dump/releases/download/chl/chl.bash
+sudo wget https://github.com/BCschwifty/CHL-command/releases/download/CHL-1.0/chl.bash
 sudo cp chl.bash chl
 sudo rm chl.bash
 sudo chmod +x chl
 sudo chmod -R 755 /scripts/run.sh
+
